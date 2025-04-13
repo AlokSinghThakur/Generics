07860842ac74f07d3d105bc1b5f27b1daebec168
alok singh thakur <alokthakur08cse@gmail.com>
2025-04-13 22:59:01 +0530

    Enums in Generics

---
+++ src/EnumTest.java
@@ -0,0 +1,12 @@
+
+enum Day{
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THRUSDAY, FRIDAY, SATURDAY
+}
+public class EnumTest {
+    private static Day day1;
+
+    public static void main(String[] args) {
+        Day day =Day.FRIDAY;
+//        Day day1 = "Monday";
+    }
+}
