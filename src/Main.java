a04b3153ed9075e5e5326dcb0ef76bc0193b28c8
alok singh thakur <alokthakur08cse@gmail.com>
2025-04-13 22:19:49 +0530

    Generic Type Safety Generic using Class Generic Using interface Generic Bound type Parameter

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+//import java.lang.reflect.Type;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Box<Integer> box = new Box<>();
+        box.setValue(1);
+        int i = box.getValue();
+        System.out.println("Hello world!" + i);
+
+//        Pair<String, Integer> pair = new Pair<>("Age", 30);
+//        System.out.println(pair.getKey());
+//        System.out.println(pair.getValue());
     }
 }
+
+//      T: Type
+//      E: Element
+//      K: Key (used in maps)
+//      V:Value (used in maps)
+//      N: Number
